97a19539b3f9f3288a0aa9d9155fe6514a82afa5
Kristian Dehn Kolbæk <kristian.kolbaek@bekey.dk>
2024-05-02 14:34:14 +0200

    build: adding some styling for full preview icon

--- resources/js/components/Medialibrary/MediaPreview.vue
+++ resources/js/components/Medialibrary/MediaPreview.vue
@@ -1,12 +1,12 @@
 <template>
   <embed
-    v-if="usePreview && media.mimeType.includes('pdf')"
+    v-if="media.mimeType.includes('pdf')"
     :src="media.previewUrl"
     type="application/pdf"
     class="block h-24 w-full object-cover"
   />
   <img
-    v-else-if="usePreview"
+    v-else-if="media.mimeType.includes('image')"
     :src="media.previewUrl"
     :alt="media.fileName"
     class="block h-24 w-full object-cover"
