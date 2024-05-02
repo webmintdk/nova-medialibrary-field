a788dc2bf47096bf25b0283179ef2aa7d1060db1
Kristian Dehn Kolbæk <kristian.kolbaek@bekey.dk>
2024-05-02 13:59:29 +0200

    Build: Fixing preview url link

--- resources/js/components/Medialibrary/MediaPreview.vue
+++ resources/js/components/Medialibrary/MediaPreview.vue
@@ -1,12 +1,12 @@
 <template>
   <embed
-    v-if="usePreview && media.mimeType.includes('pdf')"
+    v-if="media.mimeType.includes('pdf')"
     :src="media.previewUrl"
     type="application/pdf"
     class="block h-24 w-full object-cover"
   />
   <img
-    v-else-if="usePreview"
+    v-else
     :src="media.previewUrl"
     :alt="media.fileName"
     class="block h-24 w-full object-cover"
@@ -20,8 +20,16 @@
       </span>
     </slot>
   </div>
-  <div>
-    <a :href="media.downloadUrl" target="_blank" class="text-xs text-80 truncate">Se medie</a>
+  <div style="
+    position: absolute;
+    z-index: 1;
+    left: 50%;
+    margin-left: -8px;
+    margin-top: -2px;
+  ">
+    <a :href="media.previewUrl" target="_blank" class="text-xs text-80 truncate">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="18" height="18" class="inline-block" role="presentation"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg>
+    </a>
   </div>
 </template>
 
